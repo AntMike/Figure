cb80cd87d5e4a81901fd5b48245fefe437b40916
MrSherlok <mikeAntoniv@gmail.com>
2018-08-07 10:12:30 +0300

    add outputter

---
+++ Assets/Scripts/Outputter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FigureTest.Generators;
+using FigureTest.Figures;
+using UnityEngine.UI;
+
+namespace FigureTest.Outputter
+{
+    public class Outputter : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _outputTxt;
+
+        private void Start()
+        {
+            GenerateNewBtn();
+        }
+
+        public void GenerateNewBtn()
+        {
+            ClearOutputter();
+            Generator.instance.GenerateFigures();
+            List<Figure> _figures = Generator.instance.figuresBase;
+            foreach (Figure _figure in _figures)
+            {
+                AddDataToOutputter(_figure.DrawFigure(_figure.figureTypeName, _figure.FigureArea(), _figure.UniqueFunction(), _figure.FigureColor));
+            }
+        }
+
+        public void AddDataToOutputter(string _data)
+        {
+            _outputTxt.text += "\n" + _data;
+        }
+
+        public void ClearOutputter()
+        {
+            _outputTxt.text = "";
+        }
+
+    }
+}
